6ebf3f8016e23d541cfe0e156ed0b3ba616a8372
Jebimeta <jebilongo@gmail.com>
2025-06-12 21:10:48 +0200

    CustomerJpaRepository query added

--- src/main/java/com/onlineshop/repository/jpa/CustomerJpaRepository.java
+++ src/main/java/com/onlineshop/repository/jpa/CustomerJpaRepository.java
@@ -2,6 +2,8 @@ package com.onlineshop.repository.jpa;
 
 import com.onlineshop.repository.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,8 +12,10 @@ import java.util.Optional;
 @Repository
 public interface CustomerJpaRepository extends JpaRepository<Customer, Long> {
 
-	// Métodos para buscar un cliente por su nombre de usuario, correo electrónico o token de verificación
-	Optional<Customer> findByUsername(String username);
+	// Métodos para buscar un cliente por su nombre de usuario, correo electrónico o token
+	// de verificación
+	@Query("SELECT c FROM Customer c WHERE c.username = :username")
+	Optional<Customer> findByUsername(@Param("username") String username);
 
 	// Busca un cliente por su correo electrónico
 	Optional<Customer> findByEmail(String email);
