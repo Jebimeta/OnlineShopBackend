71ec4d126819c776f48810ac86c54fec8cbb4bef
Jebimeta <jebilongo@gmail.com>
2025-06-16 22:40:23 +0200

    src mapper

--- src/main/java/com/onlineshop/controller/mapper/ProductToProductResponseMapper.java
+++ src/main/java/com/onlineshop/controller/mapper/ProductToProductResponseMapper.java
@@ -12,7 +12,7 @@ import org.springframework.core.convert.converter.Converter;
 @Mapper(componentModel = "spring")
 public interface ProductToProductResponseMapper extends Converter<Product, ProductResponse> {
 
-	String BASE_URL = "https://onlineshopbackend-59zq.onrender.com/products/images/";
+	String BASE_URL = "src/main/resources/static/products/images";
 
 	@Mapping(source = "image", target = "image", qualifiedByName = "imageNameToImageUrl")
 	ProductResponse convert(@NonNull Product source);
