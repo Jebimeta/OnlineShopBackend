2e3a2e898e197945c2cd49174f3d0d0cf9e59c56
Jebimeta <jebilongo@gmail.com>
2025-06-18 22:42:14 +0200

    cambios en la direccion proporcionada por render

--- src/main/java/com/onlineshop/controller/mapper/ProductToProductResponseMapper.java
+++ src/main/java/com/onlineshop/controller/mapper/ProductToProductResponseMapper.java
@@ -12,7 +12,7 @@ import org.springframework.core.convert.converter.Converter;
 @Mapper(componentModel = "spring")
 public interface ProductToProductResponseMapper extends Converter<Product, ProductResponse> {
 
-	String BASE_URL = "https://onlineshopbackend-f98u.onrender.com/products/images/";
+	String BASE_URL = "https://onlineshopbackend-f98u.onrender.com/images/";
 
 	@Mapping(source = "image", target = "image", qualifiedByName = "imageNameToImageUrl")
 	ProductResponse convert(@NonNull Product source);
